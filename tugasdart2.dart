d84acbde28c5a9c48d45ae69f9cb0ec048b89e34
Jeihan <jeihan.azib@gmail.com>
2026-07-19 22:20:18 +0700

    tugas dart2
    tugas dart2

--- tugasdart2.dart
+++ tugasdart2.dart
@@ -1,1 +1,29 @@
-tes
+void main() {
+    // variabel
+    int uts = 70;
+    int uas = 70;
+    double kehadiran = 75;
+
+    // hitung rata rata
+    double ratarata = ("uts + uas")/2;
+
+    // syarat kelulusan
+    if(nilai > 70); {
+        print {Kategori = "Lulus"}
+    if(nilai < 70); {
+        print {Kategori = "Tidak Lulus"}
+    if(kehadiran > 75); {
+        print {Kategori = "Lulus"}
+    if(kehadiran < 75); {
+        print {Kategori = "Tidak Lulus"}
+    }
+
+    // output hasil lulus atau tidak lulus
+    if(Lulus) {
+        print {Lulus};
+        else if(Tidak Lulus) {
+            print {Tidak Lulus};
+        }
+    }
+    }
+}
